03c08a9963c37d22d089223ec1548d7cf9e2ba0a
GraziAraújo <0000111457546XSP@aluno.educacao.sp.gov.br>
2024-05-29 13:20:18 -0300

    sketch.js

---
+++ sketch.js
@@ -0,0 +1,26 @@
+gura = 450
+let altura = 300
+let xbolinha = 200
+let ybolinha = 200
+let xraquete = 5
+let yraquete = altura/2
+let xraqueteop = 433
+let yraqueteop = altura/2
+let raquetelarg = 12
+let raquetealt = 95
+let diametro = 30
+let raio = diametro/2
+let xvelocidade = 5
+let yvelocidade = 5
+let yvelocidadeop;
+let colidiu = false
+let minhapontuacao = 0
+let pontuacaoop = 0
+
+
+function setup() {
+  createCanvas(largura, altura);
+}
+
+function draw() {
+  background
